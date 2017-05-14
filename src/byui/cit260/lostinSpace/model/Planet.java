8bc7c4ae6681360184504fa6537b1559c4e61f5b
devinerickson15 <devinjohn15@gmail.com>
2017-05-14 01:21:05 -0400

    Merge origin/master

---
+++ src/byui/cit260/lostinSpace/model/Planet.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.lostinSpace.model;
+
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author devinerickson
+ */
+public class Planet implements Serializable {
+   
+    //Bean
+    private String name;
+    private String desc;
+    private int currentLocation;
+    
+    //Public 
+    public Planet(){
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public void setCurrentLocation(int currentLocation) {
+        this.currentLocation = currentLocation;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 67 * hash + Objects.hashCode(this.name);
+        hash = 67 * hash + Objects.hashCode(this.desc);
+        hash = 67 * hash + this.currentLocation;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Planet other = (Planet) obj;
+        if (this.currentLocation != other.currentLocation) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.desc, other.desc)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
