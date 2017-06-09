fbc270fb59bfc3b2a57254e557ed80b58a89e1f4
Kevin <Kevin@Megans_laptop>
2017-06-08 20:41:15 -0700

    WOrked on FuelView, cleaned up GameMenuView & HelpMenuView

--- src/byui/cit260/lostinSpace/view/HelpMenuView.java
+++ src/byui/cit260/lostinSpace/view/HelpMenuView.java
@@ -87,26 +87,26 @@ public class HelpMenuView {
 
     private void displayGoal() {
          System.out.println("The goal is to return to Earth safely by "
-                 + "navigating your way through space, fighting off "
-                 + "dangerous alien creatures, and collecting helpful "
-                 + "clues and resources to help you along the way.");
+                 + "\n navigating your way through space, fighting off "
+                 + "\n dangerous alien creatures, and collecting helpful "
+                 + "\n clues and resources to help you along the way.");
     }
 
     private void howToMove() {
         System.out.println("The player jumps from one planet to the "
-                + "other in his spaceship. The player will “look to "
-                + "the stars” to see the four planets they will be "
-                + "able to move to. Will it move them closer to or "
-                + "farther from Earth? The player doesn't know...");
+                + "\n other in his spaceship. The player will “look to "
+                + "\n the stars” to see the four planets they will be "
+                + "\n able to move to. Will it move them closer to or "
+                + "\n farther from Earth? The player doesn't know...");
     }
 
     private void displayFuelPer() {
          System.out.println("The player should check the fuel "
-                 + "percentage of the spaceship before traveling "
-                 + "to the next destination. If the fuel level "
-                 + "is less than 20%, the player will need to "
-                 + "earn more fuel before launching to a different "
-                 + "celestial body.");
+                 + "\n percentage of the spaceship before traveling "
+                 + "\n to the next destination. If the fuel level "
+                 + "\n is less than 20%, the player will need to "
+                 + "\n earn more fuel before launching to a different "
+                 + "\n celestial body.");
     }
 
     private void returnMainMenu() {
