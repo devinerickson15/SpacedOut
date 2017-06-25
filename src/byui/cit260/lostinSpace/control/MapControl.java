36f0e92b64c2f0eb5447083333221cadde669dc2
Kevin <Kevin@Megans_laptop>
2017-06-25 13:53:26 -0700

    6/25 Added some planets

--- src/byui/cit260/lostinSpace/control/MapControl.java
+++ src/byui/cit260/lostinSpace/control/MapControl.java
@@ -18,7 +18,7 @@ class MapControl {
     
     Map map = new Map();
     
-    Planet[] locations = new Planet[8];
+    Planet[] locations = new Planet[16];
     
     /*private String name;
     private String desc;
@@ -34,6 +34,14 @@ class MapControl {
     locations[5] = new Planet("Mars" , "Dry and barren", 5, 29, 15);
     locations[6] = new Planet("Moon", "Hey, I can see my house from here", 6, 22, 12);
     locations[7] = new Planet("Pluto", "Did you hear about Pluto?! That's messed up!", 7, 33, 18);
+    locations[8] = new Planet ("Fiesta", "A party around every crater.", 8, 16, 28);
+    locations[9] = new Planet ("Orblook", "A perfectly round planet", 9, 17, 20);
+    locations[10] = new Planet ("Zarthon", "Made of crystals (not crystal meth).", 10, 31, 12);
+    locations[11] = new Planet ("Athenia", "Watch out for the teenagers.", 11 , 25, 22);
+    locations[12] = new Planet ("Draconia", "A harsh place to make a home.", 12, 19, 29);
+    locations[13] = new Planet ("Blowhard", "High heat and strong winds", 13, 45, 22); //My mother-in-law named this one :)
+    locations[14] = new Planet ("Enano", "Beware the dwarf!", 14, 40, 21);
+    locations[15] = new Planet ("Niqueetun", "Don't get caught in the curls!", 15, 42, 19);
    
     
     assignScenesToLocations(locations);
