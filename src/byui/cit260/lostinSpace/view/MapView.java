cec870ce219f163b4c6b0503040b28e70bad89ad
Keith Jacobson <JacobsonKe@0103075FXA.edmonds.wednet.edu>
2017-06-24 21:51:46 -0700

    6/23/17 - Did some more stuff, have a bit more to do to see if I can get the map to work.

---
+++ src/byui/cit260/lostinSpace/view/MapView.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.lostinSpace.view;
+
+/**
+ *
+ * @author JacobsonKe
+ */
+public class MapView {
+    
+    
+    
+    /*private void displayMap(){
+        Game game = LostInSpace.getCurrentGame();
+        Map map = game.getMap();
+        Planet[] locations = map.getPlanets();
+        
+        for(int i=0; i < locations.length; i++){
+            System.out.println(
+            "Planet " + locations[i].getName() + ": "
+            + locations[i].getScene().getDescription());
+        }
+    }*/
+}
